b045915e50e8309630cfbd7415480ce460555fcb
Rhi4nn3 <sofiarey6309@gmail.com>
2026-07-07 16:57:03 +0800

    performance task

---
+++ Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+     public static void main(String[] args) {
+      System.out.println("_,.-'~'-.,__,.-'~'-.,__,.-'~'-.,__,.-'~'-.,__,.-'~'-.,_");
+      System.out.println("           WELCOME TO PWU-JASMS, NEW STUDENTS!");
+      System.out.println("_,.-'~'-.,__,.-'~'-.,__,.-'~'-.,__,.-'~'-.,__,.-'~'-.,_");
+      System.out.println("Welcome, New Student! This is PWU-JASMS, where you will spend your Senior high School. I hope you'll have a wonderful time.");
+      System.out.println("It always seem impossible until it's done");
+      System.out.println("The teacher for ICT is Teacher Lan Sevilleno");
+      System.out.println("Life has no remote. Get up and go change it yourself");
+      //->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->
+      //->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->
+      //->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->
+       System.out.println("_,.-'~'-.,__,.-'~'-.,__,.-'~'-.,__,.-'~'-.,__,.-'~'-.,_");
+        System.out.println("              PERSONAL INFORMATION SECTION");
+         System.out.println("_,.-'~'-.,__,.-'~'-.,__,.-'~'-.,__,.-'~'-.,__,.-'~'-.,_");
+        System.out.println("My name is Rhianne Sofia Rey");
+        System.out.println("Grade 12, ICT strand");
+        System.out.println("Chicken Nuggets");
+        System.out.println("I chose ICT because I have a passion for computers");
+        System.out.println("My dream job is to be a programmer");
+        System.out.println("Don't give up if something looks hard, keep going till the end");
+        System.out.println("Programmer");
+        System.out.println("Programmer");
+        System.out.println("Programmer");
+    }
+}
+
