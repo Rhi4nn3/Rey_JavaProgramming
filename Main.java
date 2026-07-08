d2b5b8707763609f38c16ac1baf8c56fc0ccc157
Rhi4nn3 <sofiarey6309@gmail.com>
2026-07-08 16:09:24 +0800

    performance task(done)

--- Main.java
+++ Main.java
@@ -2,27 +2,36 @@ public class Main {
 
      public static void main(String[] args) {
       System.out.println("_,.-'~'-.,__,.-'~'-.,__,.-'~'-.,__,.-'~'-.,__,.-'~'-.,_");
-      System.out.println("           WELCOME TO PWU-JASMS, NEW STUDENTS!");
+      System.out.println("         | WELCOME TO PWU-JASMS, NEW STUDENTS! |");
       System.out.println("_,.-'~'-.,__,.-'~'-.,__,.-'~'-.,__,.-'~'-.,__,.-'~'-.,_");
-      System.out.println("Welcome, New Student! This is PWU-JASMS, where you will spend your Senior high School. I hope you'll have a wonderful time.");
-      System.out.println("It always seem impossible until it's done");
-      System.out.println("The teacher for ICT is Teacher Lan Sevilleno");
-      System.out.println("Life has no remote. Get up and go change it yourself");
+      System.out.println("  ");
+      System.out.println("▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌");
+      System.out.println("▌Welcome, New Student! This is PWU-JASMS, where you will spend your Senior high. I hope you have a wonderful time.  ▌");  
+      System.out.println("▌Ouote: It always seem impossible until it's done                                                                   ▌");
+      System.out.println("▌Teacher Lan Sevilleno                                                                                              ▌");
+      System.out.println("▌Quote: Life has no remote. Get up and go change it yourself                                                        ▌");
+      System.out.println("▐▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌");
+      System.out.println("  ");
       //->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->
       //->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->
       //->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->
-       System.out.println("_,.-'~'-.,__,.-'~'-.,__,.-'~'-.,__,.-'~'-.,__,.-'~'-.,_");
-        System.out.println("              PERSONAL INFORMATION SECTION");
-         System.out.println("_,.-'~'-.,__,.-'~'-.,__,.-'~'-.,__,.-'~'-.,__,.-'~'-.,_");
-        System.out.println("My name is Rhianne Sofia Rey");
-        System.out.println("Grade 12, ICT strand");
-        System.out.println("Chicken Nuggets");
-        System.out.println("I chose ICT because I have a passion for computers");
-        System.out.println("My dream job is to be a programmer");
-        System.out.println("Don't give up if something looks hard, keep going till the end");
-        System.out.println("Programmer");
-        System.out.println("Programmer");
-        System.out.println("Programmer");
+        System.out.println("_,.-'~'-.,__,.-'~'-.,__,.-'~'-.,__,.-'~'-.,__,.-'~'-.,_");
+        System.out.println("           | PERSONAL INFORMATION SECTION |");
+        System.out.println("_,.-'~'-.,__,.-'~'-.,__,.-'~'-.,__,.-'~'-.,__,.-'~'-.,_");
+        System.out.println("  ");
+        System.out.println("▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌");
+        System.out.println("▌My name is Rhianne Sofia Rey                                                                     ▌");
+        System.out.println("▌Grade 12, ICT strand                                                                             ▌");
+        System.out.println("▌For me, ICT is a way for me to express my creativity and experiment with different code          ▌");
+        System.out.println("▌I chose ICT because I have an interest for computers                                             ▌");
+        System.out.println("▌My dream job is to be a programmer                                                               ▌");
+        System.out.println("▌Quote: Don't give up if something looks hard, keep going till the end                            ▌");
+        System.out.println("▌      \\    /\\                                                                                    ▌");
+        System.out.println("▌       )  ( ')                                                                                   ▌");
+        System.out.println("▌      (  /  )                                                                                    ▌");
+        System.out.println("▌       \\(__)|                                                                                    ▌");
+        System.out.println("▐▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌");
+        /*End of Personal Information Section */
     }
 }
 
