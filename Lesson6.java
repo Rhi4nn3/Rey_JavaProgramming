e4eb6d6f31affed95a392b4eb7da44c3036acad8
Rhi4nn3 <sofiarey6309@gmail.com>
2026-07-16 18:15:44 +0800

    student info

---
+++ Lesson6.java
@@ -0,0 +1,22 @@
+public class Lesson6 {
+    public static void main(String[] args) {
+       String studentName = "Rhianne Rey";
+       int studentAge = 17;
+       int gradeLevel = 12;
+       String studentSection = "Aquinas";
+       String studentSchool = "PWU - JASMS";
+       String favoriteSubject = "English";
+       int generalAverage = 92;
+       boolean honorStudent = generalAverage >= 90;
+
+       System.out.println("STUDENT INFORMATION");
+       System.out.println("Student Name: " + studentName);
+       System.out.println("Student Age: " + studentAge);
+       System.out.println("Grade Level: " + gradeLevel);
+       System.out.println("Student Section: " + studentSection);
+       System.out.println("Student School: " + studentSchool);
+       System.out.println("Favorite Subject: " + favoriteSubject);
+       System.out.println("General Average: " + generalAverage);
+       System.out.println("Honor Student: " + honorStudent);
+    }
+}
