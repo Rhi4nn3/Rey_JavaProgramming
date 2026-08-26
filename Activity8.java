3bfc34a53023fd69409a9b2b617b4ab1dba49bad
Rhi4nn3 <sofiarey6309@gmail.com>
2026-08-26 11:54:33 +0800

    day of the week

---
+++ Activity8.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Activity8 {
+    public static void main(String[] args) {
+     Scanner scn = new Scanner(System.in);
+
+     System.out.println("What day of the week is today? (1, 2, 3, 4 , 5 , or 6/7)");
+     int num1 = scn.nextInt();
+
+     if(num1 == 1){
+        System.out.println("Today is a Monday !!");
+     }
+     else if (num1 == 2){
+        System.out.println("Today is Tuesday !!");
+     }
+
+     
+     else if (num1 == 3){
+        System.out.println("Today is Wednesday !!");
+     }
+
+    
+     else if (num1 == 4){
+        System.out.println("Today is Thursday !!");
+     }
+
+    
+     else if (num1 == 5){
+        System.out.println("Today is Friday !!");
+     }
+
+  
+     else if (num1 == 6){
+        System.out.println("Today is the Weekend (Saturday) !!");
+     }
+
+    else if (num1 == 7){
+        System.out.println("Today is the Weekend (Sunday) !!");
+     }
+
+     else if (num1 >7){
+        System.out.println ("There are only seven days of the week.");
+     }
+    }
+}
---
+++ Lesson10.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Lesson10 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+
+        
+
+    //     System.out.println("Are you a filipino citizen? (Y/N)");
+    //     char nationality= scn.next().charAt();
+    //     System.out.println("please enter your age:");
+    //     int age = scn.nextInt();
+
+    //    if(age >=18 && nationality == "Y"){
+    //     System.out.println("you can vote");
+    //    }
+
+
+
+        // System.out.println("please enter a number");
+        // int num1 = scn.nextInt();
+
+        // if(num1 >=0){
+        //     System.out.println(num1 + " = positive number");
+        // }
+
+        // else{
+        //     System.out.println(num1 + " = negative number");
+        // }
+        // scn.close();
+    }
+}
